8f4431bac0b85c8eaea29770aeef5937ae0556d2
Валерия <90615128+okshinaa@users.noreply.github.com>
2021-09-29 15:33:33 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,26 +7,57 @@ public class Main {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
-        System.out.println("Введите стороны треуголька:");
-        Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-        double c = scanner.nextDouble();
-        calculanyions(a, b, c);
 
+        double a = readTriangleSide("A");
+        double b = readTriangleSide("B");
+        double c = readTriangleSide("C");
+
+        boolean flag = doesTriangleExists(a,b,c);
+        if(!flag) {
+            System.out.println("Треугольник не существует");
+            System.exit(0);
+        }
+
+        double[] heights = calculateTriangleHeights(a, b, c);
+
+        printTriangleHeights(heights);
     }
 
-    public static void calculanyions(double a, double b, double c) {
+    private static double readTriangleSide(String sideName) {
+        System.out.println("Введите сторону треуголька " + sideName);
+        Scanner scanner = new Scanner(System.in);
+        double side = scanner.nextDouble();
 
-        if (a < (b + c) && b < (a + c) && c < (a + b)) {
-            double p = (a + b + c) / 2;
-            double k = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-            double ha = 2 / a * k;
-            double hb = 2 / b * k;
-            double hc = 2 / c * k;
-            System.out.printf("Высоты треугольника равны:%1$.3f, %2$.3f, %3$.3f", ha, hb, hc);
-        } else {
-            System.out.println("Неравенство треугольника не выполняется");
+        if (side <= 0) {
+            System.out.println("Данные не корректны, введенное значение меньше 0");
+            System.exit(0);
         }
+
+        return side;
+    }
+
+    private static boolean doesTriangleExists(double a, double b, double c){
+        return (a < (b + c) && b < (a + c) && c < (a + b));
+    }
+
+    private static double[] calculateTriangleHeights(double a, double b, double c) {
+        double[] heights = new double[3];
+
+        double p = (a + b + c) / 2;
+        double k = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        double ha = 2 / a * k;
+        double hb = 2 / b * k;
+        double hc = 2 / c * k;
+
+        heights[0] = ha;
+        heights[1] = hb;
+        heights[2] = hc;
+
+        return heights;
+    }
+
+    private static void printTriangleHeights(double[] heights){
+        System.out.printf("Высоты треугольника равны: %1$.3f, %2$.3f, %3$.3f", heights[0], heights[1], heights[2]);
     }
 }
